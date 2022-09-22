6213757d11d354d4ea698b1443c3aa08d853e9ab
308 <308@academicos.cesde.edu.co>
2022-09-22 08:44:59 -0500

    cambios

--- graficaRectangulo.py
+++ graficaRectangulo.py
@@ -1,7 +1,10 @@
 
-
-def grafica(alto,ancho):
+alto=3
+ancho=5
+def grafica(ancho,alto):
     for i in range(alto):
         print("")
         for i in range(ancho):
             print("*", end="")
+
+grafica(ancho, alto)
